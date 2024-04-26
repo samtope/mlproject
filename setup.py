6fa0a83ad9de55319bf1bde309c5db5b3babad6d
samtope <oluwatemitopesam@gmail.com>
2024-04-26 19:09:46 +0100

    All change

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 from setuptools import find_packages,setup
 from typing import List
 
-from setuptools.sandbox import save_path
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-HYPEN_E_DOT='-e  .'
-def get_requirements(file_patth:str)->List[str]:
-      '''
-      This function will return the list of requirements 
-      '''  
-def process_requirements(save_path):
-    requirements = []
-    with open(save_path) as file_obj:
-        requirements = file_obj.readline().strip().split(',')
-        requirements = [req.strip() for req in requirements]
-
-        if "HYPEN_E_DOT" in requirements:
-            requirements.remove("HYPEN_E_DOT")
-
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-    name='mlproject2',
-    version='0.0.1',
-    author='sam', 
-    author_email='oluwatemitopesam@gamil.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.text')
+name='mlproject',
+version='0.0.1',
+author='Krish',
+author_email='krishnaik06@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
 
-   ) 
+)
